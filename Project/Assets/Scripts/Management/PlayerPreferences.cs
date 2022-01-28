31d1ffc8cc8c771eaaee7a720bc9abecd2fb775d
Jordi Alba <44704875+jalba002@users.noreply.github.com>
2022-01-28 16:39:04 +0100

    Data & Menu

---
+++ Project/Assets/Scripts/Management/PlayerPreferences.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class PlayerPrefsLoader : MonoBehaviour
+{
+    private PlayerPrefsLoader _playerPreferences;
+
+    public PlayerPrefsLoader Instance
+    {
+        get => _playerPreferences;
+        set
+        {
+            if (_playerPreferences != null)
+            {
+                Destroy(this);
+                return;
+            }
+
+            _playerPreferences = value;
+        }
+    }
+
+    public string[] audioVariables =
+    {
+        "MasterVolume",
+        "SoundsVolume",
+        "MusicVolume",
+        "AmbienceVolume",
+        "CinematicsVolume"
+    };
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        LoadAudioPreferences();
+    }
+
+    public void SaveAudioPreferences()
+    {
+        Debug.Log("Saving prefs");
+        foreach (var frase in audioVariables)
+        {
+            StoreFloat(frase);
+        }
+        PlayerPrefs.Save();
+    }
+
+    private void StoreFloat(string variableName)
+    {
+        AudioManager.Instance.GetMixerVariable(variableName, out float variable);
+        PlayerPrefs.SetFloat(variableName, variable);
+    }
+
+    private float RecoverFloat(string variableName)
+    {
+        return PlayerPrefs.GetFloat(variableName);
+    }
+
+    public void LoadAudioPreferences()
+    {
+        Debug.Log("Loading prefs");
+        foreach (var frse in audioVariables)
+        {
+            AudioManager.Instance.SetMixerVariable(frse, RecoverFloat(frse));
+        }
+    }
+}
---
+++ Project/Assets/Scripts/UI/MainMenu/MainMenuManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public string sceneToLoad = "01";
+
+    public RectTransform MainParent;
+    public RectTransform ControlsParent;
+    public RectTransform OptionsParent;
+    public RectTransform CreditsParent;
+
+    private InputSystemUIInputModule InputSystemUI;
+
+    private void Start()
+    {
+        ReturnToMain();
+        InputSystemUI = FindObjectOfType<InputSystemUIInputModule>();
+        InputSystemUI.cancel.action.performed += HandleReturn;
+    }
+
+    public void ToggleOptions()
+    {
+        MainParent.gameObject.SetActive(OptionsParent.gameObject.activeInHierarchy);
+        OptionsParent.gameObject.SetActive(!OptionsParent.gameObject.activeInHierarchy);
+    }
+
+    public void ToggleControls()
+    {
+        MainParent.gameObject.SetActive(ControlsParent.gameObject.activeInHierarchy);
+        ControlsParent.gameObject.SetActive(!ControlsParent.gameObject.activeInHierarchy);
+    }
+
+    public void ToggleCredits()
+    {
+        MainParent.gameObject.SetActive(CreditsParent.gameObject.activeInHierarchy);
+        CreditsParent.gameObject.SetActive(!CreditsParent.gameObject.activeInHierarchy);
+    }
+
+    private void HandleReturn(InputAction.CallbackContext context)
+    {
+        ReturnToMain();
+    }
+
+    public void ReturnToMain()
+    {
+        MainParent.gameObject.SetActive(true);
+        OptionsParent.gameObject.SetActive(false);
+        CreditsParent.gameObject.SetActive(false);
+        ControlsParent.gameObject.SetActive(false);
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void ExitApplication()
+    {
+        if (Application.isPlaying)
+        {
+            Application.Quit();
+        }
+    }
+}
