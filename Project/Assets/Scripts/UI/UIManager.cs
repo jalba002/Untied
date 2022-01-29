30976e8dbb619b53318c4e220a06449131f6d1d6
Jordi Alba <44704875+jalba002@users.noreply.github.com>
2022-01-29 05:42:40 +0100

    Expansion and UI

---
+++ Project/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class UIManager : MonoBehaviour
+{
+    public string sceneName = "UI";
+
+    private IEnumerator loadSceneCoroutine;
+
+    [Button("Load UI Scene")]
+    public void Load()
+    {
+        AsyncOperation sceneLoadOperation;
+        if (!SceneManager.GetSceneByName(sceneName).isLoaded)
+            sceneLoadOperation = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        else
+            SceneManager.UnloadSceneAsync(sceneName);
+    }
+}
