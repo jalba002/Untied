79981498cd88ac8fc1eae7a554435c445f0466e9
Jordi Alba <44704875+jalba002@users.noreply.github.com>
2022-01-30 03:04:24 +0100

    Fixes

--- Project/Assets/Scripts/UI/UIManager.cs
+++ Project/Assets/Scripts/UI/UIManager.cs
@@ -6,8 +6,14 @@ using System.Collections;
 public class UIManager : MonoBehaviour
 {
     public string sceneName = "UI";
+    public bool loadOnStart = false;
 
     private IEnumerator loadSceneCoroutine;
+
+    private void Start()
+    {
+        if (loadOnStart) Load();
+    }
 
     [Button("Load UI Scene")]
     public void Load()
